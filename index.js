2add7b9065c7b30ef725229d55bdbce077e09ae6
Nick Momrik <nick@momrik.com>
2016-02-12 14:29:29 -0500

    Very simple logic to go all the way up and then all the way down, stopping at each floor. Passes challenge #1.

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 {
   init: function(elevators, floors) {
+    var floorCount = floors.length;
+
+    function elevatorCanGoUp( elevator, floorNum = null ) {
+      if (!floorNum) {
+        floorNum = elevator.currentFloor();
+      }
+
+      return (floorCount - 1) > floorNum;
+    }
+
+    function elevatorCanGoDown( elevator, floorNum = null ) {
+      if (!floorNum) {
+        floorNum = elevator.currentFloor();
+      }
+
+      return 0 < floorNum;
+    }
+
     floors.forEach(function(floor) {
       floor.on("up_button_pressed", function() {
 
@@ -12,11 +30,23 @@
 
     elevators.forEach(function(elevator){
       elevator.on("idle", function() {
-
+        if (elevator.goingUpIndicator() && elevatorCanGoUp(elevator)) {
+          elevator.goingDownIndicator(false);
+          elevator.goToFloor(elevator.currentFloor() + 1);
+        } else if (elevator.goingDownIndicator() && elevatorCanGoDown(elevator)) {
+          elevator.goingUpIndicator(false);
+          elevator.goToFloor(elevator.currentFloor() - 1);
+        }
       });
 
       elevator.on('stopped_at_floor', function(floorNum) {
-
+        if (elevator.goingUpIndicator() && !elevatorCanGoUp(elevator, floorNum)) {
+          elevator.goingUpIndicator(false);
+          elevator.goingDownIndicator(true);
+        } else if (elevator.goingDownIndicator() && !elevatorCanGoDown(elevator, floorNum)) {
+          elevator.goingUpIndicator(true);
+          elevator.goingDownIndicator(false);
+        }
       });
 
       elevator.on("passing_floor", function(floorNum, direction) {
