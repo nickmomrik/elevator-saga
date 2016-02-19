9e65785908488b55118d42ec74b9948387372762
Nick Momrik <nick@momrik.com>
2016-02-19 14:07:36 -0500

    debug: add some more console logging and fix up spacing

--- index.js
+++ index.js
@@ -66,13 +66,13 @@
 
 		floors.forEach( function( floor ) {
 			floor.on( "up_button_pressed", function() {
-				//console.log('up', floor.floorNum());
+				//console.log( 'up', floor.floorNum() );
 
 				addToWaitingQueue( floor.floorNum() );
 			} );
 
 			floor.on( "down_button_pressed", function() {
-				//console.log('', floor.floorNum());
+				//console.log( 'down', floor.floorNum() );
 
 				addToWaitingQueue( floor.floorNum() );
 			} );
@@ -80,7 +80,7 @@
 
 		elevators.forEach( function( elevator ){
 			elevator.on( "idle", function() {
-				//console.log('idle');
+				// console.log( 'idle', elevator.destinationQueue, waitingQueue, elevator.loadFactor(), elevator.goingUpIndicator(), elevator.goingDownIndicator() );
 
 				resetIndicators( elevator );
 
@@ -90,18 +90,21 @@
 				}
 
 				if ( 0 == elevator.destinationQueue.length && waitingQueue.length ) {
+					//console.log( 'idle - going to waitingQueue floor' );
 					elevator.goToFloor( waitingQueue.shift() );
 				}
 
 				if ( elevator.goingUpIndicator() ) {
+					//console.log( 'idle - going to next up floor' );
 					elevator.goToFloor( elevator.currentFloor() + 1 );
 				} else if ( elevator.goingDownIndicator() ) {
+					//console.log( 'idle - going to next down floor' );
 					elevator.goToFloor( elevator.currentFloor() - 1 );
 				}
 			} );
 
 			elevator.on( 'stopped_at_floor', function( floorNum ) {
-				//console.log('stopped', floorNum);
+				//console.log( 'stopped', floorNum );
 
 				resetIndicators( elevator );
 				resetDestinationQueue( elevator );
@@ -109,12 +112,14 @@
 			} );
 
 			elevator.on( "passing_floor", function( floorNum, direction ) {
-				//console.log('passing', floorNum, direction);
+				//console.log( 'passing', floorNum, direction );
 
 				if ( -1 != elevator.getPressedFloors().indexOf( floorNum ) ) {
+					//console.log( 'passing - going to pressed floor', floorNum );
 					elevator.goToFloor( floorNum, true );
 				} else {
 					if ( elevator.loadFactor() < 1 && floors[floorNum].buttonStates[direction] ) {
+						//console.log( 'passing - going to waiting floor', floorNum, direction );
 						elevator.goToFloor( floorNum, true );
 
 						// Remove from waiting queue unless someone is waiting to go in the opposite direction
@@ -126,7 +131,7 @@
 			} );
 
 			elevator.on( "floor_button_pressed", function( floorNum ) {
-				//console.log('pressed', floorNum);
+				//console.log( 'pressed', floorNum );
 
 				elevator.goToFloor( floorNum );
 			} );
